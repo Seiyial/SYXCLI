41a400ca23532ab9cfc96fe305d9e1e950764634
Seiyial <seiyial@gmail.com>
2018-02-21 14:10:50 +0800

    Add some stuff

--- Scripts/aliases/Ruby.sh
+++ Scripts/aliases/Ruby.sh
@@ -2,7 +2,20 @@
 alias bx='bundle exec'
 alias bi='bundle install'
 
+# Rake
+alias rk='rake'
+
 # Hanami
 alias ha='bundle exec hanami'
 alias hadb='HANAMI_ENV=test bundle exec hanami db prepare && bundle exec hanami db prepare'
 alias rkt='rake test'
+
+# Rails
+alias rr='clear && tput setaf 9 && echo "
+    Rails Server
+    
+=========================" && rails server'
+alias rdbrs='rails db:reset'
+alias rdbm='rails db:migrate'
+alias rdbss='rails db:seed'
+alias r='rails'
---
+++ Scripts/aliases/Yarn.sh
@@ -0,0 +1,8 @@
+alias yy='tput setaf 45 && clear && echo "==================================       
+    yarn run
+==================================
+" && yarn run'
+
+alias yi='tput setaf 45 && clear && echo "==================================       
+    yarn install
+==================================" && yarn install'
