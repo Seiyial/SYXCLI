8a2da8b6e9fcf54a67cf9d3e6fdef966b3f7de52
Seiyial <seiyial@gmail.com>
2018-05-07 08:47:32 +0800

    Yarn, git, SJ, Mix

--- Scripts/aliases/Yarn.sh
+++ Scripts/aliases/Yarn.sh
@@ -1,12 +1,38 @@
-alias yy='tput setaf 45 && clear && echo "==================================       
-    yarn run
+alias yy='tput setaf 75 && clear && echo "==================================       
+    💧  yarn run 💧
 ==================================
 " && yarn run'
 
-alias yi='tput setaf 45 && clear && echo "==================================       
-    yarn install
+alias yi='tput setaf 75 && clear && echo "==================================       
+    💧  yarn install 💧
 ==================================" && yarn install'
 
-alias ys='tput setaf 45 && clear && echo "==================================       
-    yarn install
+alias ys='tput setaf 75 && clear && echo "==================================       
+    💧  yarn start 💧
 ==================================" && yarn start'
+
+alias yys='tput setaf 75 && clear && echo "==================================       
+    💧  yarn start 💧
+==================================" && yarn start'
+
+alias yis='tput setaf 75 && clear && echo "==================================       
+    💧  yarn install 💧
+==================================" && yarn install && tput setaf 75 && clear && echo "==================================       
+    💧  yarn start 💧
+==================================" && yarn start'
+
+alias yya='tput setaf 75 && clear && echo "==================================       
+    💧  yarn add 💧
+==================================" && yarn add'
+
+alias yyga='tput setaf 75 && clear && echo "==================================       
+    💧  yarn global add 💧
+==================================" && yarn global add'
+
+alias yyda='tput setaf 75 && clear && echo "==================================       
+    💧  yarn add --dev 💧
+==================================" && yarn add --dev'
+
+alias yyr='tput setaf 75 && clear && echo "==================================       
+    💧  yarn remove 💧
+==================================" && yarn remove'
---
+++ scripts/aliases/Mix.sh
@@ -0,0 +1,4 @@
+alias mdg='mix deps.get'
+alias mect='mix ecto.create'
+alias meld='mix ecto.load'
+alias m='mix'
